154e44037ecf0621d98c683b01a2681870400093
ramondeleonca <ramondeleonca@gmail.com>
2024-07-19 18:54:42 -0600

    Added cool intro animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./index.scss";
 import gsap from "gsap";
 
+const preloadImages = (srcArray: string[]) => {
+  const promises = srcArray.map((src) => {
+    return new Promise<HTMLImageElement>((resolve, reject) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = () => resolve(img);
+      img.onerror = reject;
+    });
+  });
+  return Promise.all(promises);
+};
+
 export default function App() {
+  // Renderer ref
+  const introRendererRef = useRef<HTMLCanvasElement>(null);
+
+  // Load all images
+  const [images,  setImages] = useState<HTMLImageElement[]>([]);
+  useMemo(() => {preloadImages(Array(30).fill(0).map((_, i) => `/animations/sequence/intro/${(i + 1).toString().padStart(4, "0")}.png`)).then(setImages)}, []);
+
+  useEffect(() => {
+    console.log(images);
+  }, [images]);
+
   useEffect(() => {
     const ctx = gsap.context(() => {
-      gsap.timeline({
+      const tl1 = gsap.timeline({
         scrollTrigger: {
           trigger: ".intro",
           scrub: 1,
@@ -13,13 +36,30 @@ export default function App() {
           start: "top top",
           end: "bottom center"
         }
-      })
-      .fromTo(".b-comfy", { opacity: 0.25, translateY: 50, scale: 0.5 }, { opacity: 1, scale: 1, translateY: 0 })
-      
+      });
+
+      const tl2 = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".intro",
+          scrub: 1,
+          markers: true,
+          start: "top top",
+          end: "bottom center"
+        }
+      });
+
+      tl2.fromTo(".b-comfy", { opacity: 0.25, translateY: 50, scale: 0.5 }, { opacity: 1, scale: 1, translateY: 0 }, 0);
+      tl2.fromTo(".renderer", { filter: "brightness(0.5)", translateY: 0 }, { filter: "brightness(1)", translateY: "25%" }, 0)
+
+      for (const [index, image] of images.entries()) tl1.call(() => {
+        const canvasCtx = introRendererRef.current?.getContext("2d");
+        canvasCtx?.clearRect(0, 0, introRendererRef.current?.width ?? 0, introRendererRef.current?.height ?? 0);
+        canvasCtx?.drawImage(image, 0, 0);
+      }, [], index);
     });
 
     return () => ctx.revert();
-  }, []);
+  }, [images]);
 
   return (
     <div className='w-full h-full bg-black intro'>
@@ -27,6 +67,7 @@ export default function App() {
         <div className="flex w-full flex-col items-center">
           <h1 className="text-6xl text-white font-nexa font-bold w-fit mb-8 be-safe">Be Safe, </h1>
           <h1 className="text-6xl text-white font-rounded w-fit b-comfy">B-Comfy</h1>
+          <canvas ref={introRendererRef} width={720} height={720} className="aspect-square absolute outline-green-500 w-4/5 renderer mt-8"></canvas>
         </div>
       </div>
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (
-        <nav className="fixed top-3 left-3 right-3 p-2 rounded-2xl bg-white bg-opacity-25 backdrop-blur-lg">
+        <nav className="fixed top-3 left-3 right-3 p-2 rounded-2xl bg-white bg-opacity-25 backdrop-blur-lg z-[100]">
             <img src="/logos/logo.png" alt="" className="w-12 aspect-square" />
         </nav>
     )
